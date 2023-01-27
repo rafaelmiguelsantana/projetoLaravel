5a1b1032a6c27844a657fb09c09d05b22bbba07e
Rafael Santana <santana.destacio@gmail.com>
2023-01-27 11:12:05 -0300

    upload da pasta

---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return "login"; })->name('site.login');
+
+Route::prefix('/app')->group( function(){
+    Route::get('/clientes', function(){ return "clientes"; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return "fornecedores"; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return "produtos"; })->name('app.produtos');
+});
+
+Route::fallback( function(){
+    echo 'A rota acessada não existe. <a href=" ' . route('site.index') . '">Clique aqui</a> para ir para a página inicial';
+});
+
+
+/* verbos http na web
+get
+post
+put
+patch
+delete
+options
+
+*/
